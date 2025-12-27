001a52dd1f20aa5bf65bb089bcca0ea5eb6b255c
Yuuichiro <107589801+magnilo@users.noreply.github.com>
2025-12-27 12:04:10 +0700

    yes

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title', 'slug', 'content', 'image', 'is_published', 'user_id', 'category_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ isset($title) ? $title . ' - ' . config('app.name', 'Manifest') : config('app.name', 'Manifest') }}</title>
         <link rel="icon" href="{{ asset('logo.svg') }}" type="image/svg+xml">
 
         <!-- Fonts -->
